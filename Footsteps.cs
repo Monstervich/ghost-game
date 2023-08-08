71a9529affac88e6d4bc5c03a8e39f715c0f3e18
AA <abdullah.valve@gmail.com>
2023-08-08 14:14:26 -0500

    Ect-Scripts

---
+++ Footsteps.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour {
+
+	public AudioClip[] sounds;
+
+	void OnTriggerEnter (Collider other) { //Only works for triggers
+		Debug.Log("Detected " + other.name);
+		if (other.CompareTag ("Character")) {
+			other.SendMessage ("SetSounds", this);
+		}
+	}
+
+	public void PlaySound () { //Only works for charcters
+		GetComponent <AudioSource> ().PlayOneShot (sounds[Random.Range(0,sounds.Length)]);
+	}
+
+	public void SetSounds (Footsteps newSounds) { //Only works for charcters
+		Debug.Log("Updating sounds for " + newSounds.name);
+		sounds = newSounds.sounds;
+	}
+}
---
+++ GUIUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIUnitTests : MonoBehaviour {
+	
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+}

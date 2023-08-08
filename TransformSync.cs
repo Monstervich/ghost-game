534981e977e5fd350b00b179740baf9052664936
AA <abdullah.valve@gmail.com>
2023-08-08 14:12:49 -0500

    Multiplayer-Scripts

---
+++ TransformSync.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TransformSync : NetworkTransform {
+
+	public Vector3 RealPos;
+	public Quaternion RealRot;
+	public float SpeedOfInterpolationOfPos = 10;
+	public float SpeedOfInterpolationOfRot = 5;
+
+	public override void OnDeserialize (NetworkReader reader, bool initialState) {
+		//base.OnDeserialize (reader, initialState);
+		if (!isLocalPlayer) {
+			Transform incomingTransform = reader.ReadTransform ();
+			RealPos = incomingTransform.position;
+			RealRot = incomingTransform.rotation;
+		}
+	}
+
+	public override bool OnSerialize (NetworkWriter writer, bool initialState) {
+		//return base.OnSerialize (writer, initialState);
+		if (isLocalPlayer) {
+			writer.Write (transform);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	void LateUpdate () {
+		if (!isLocalPlayer) {
+			transform.position = Vector3.Lerp (transform.position,RealPos,Time.deltaTime*SpeedOfInterpolationOfPos);
+			transform.rotation = Quaternion.Lerp (transform.rotation, RealRot, Time.deltaTime * SpeedOfInterpolationOfRot);
+		}
+	}
+}
---
+++ myLobbyPlayer.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Collections.Generic;
+using Prototype.NetworkLobby;
+
+//USELESS, it has a lot of commented debugs. I think you can remove them.
+public class myLobbyPlayer : LobbyPlayer {
+
+	public enum Teams {
+		None,
+		Spectator,
+		Survivor,
+		Ghost
+	}
+
+	[SyncVar]
+	public Teams team = Teams.None;
+
+	public override void OnClientEnterLobby () {
+		base.OnClientEnterLobby ();
+		if (NetworkServer.active) {
+			try {
+				//Debug.Log (playerName + " is connected, chosing a team.");
+				//player.GetComponent<myLobbyPlayer> ().SetTeam (myLobbyPlayer.Teams.Survivor);
+				float Chance = Random.Range (0f, 1f);
+				//Debug.Log (playerName + "'s chance is " + Mathf.Round (Chance * 100f) / 100f);
+				bool MainConditionForGhost = false;
+				//MainConditionForGhost = LobbyPlayerList._instance.GetListOfLobbyPlayers ().Count < FindObjectOfType<LobbyManager> ().maxPlayers ? !CheckForGhost () && Chance < 0.5f : true;
+				if (LobbyPlayerList._instance.GetListOfLobbyPlayers ().Count < FindObjectOfType<LobbyManager> ().maxPlayers) {
+					//Debug.Log("There is still space in the server. Choosing a ghost...");
+					MainConditionForGhost = !CheckForGhost () && Chance < 0.5f;
+				} else {
+					MainConditionForGhost = !CheckForGhost ();
+					//Debug.Log("There is not space in the server. Ghost or Survivor?");
+				}
+				/*Debug.Log (playerName + "'s MainConditionForGhost is " + MainConditionForGhost.ToString () 
+					+ "\nMore info about LobbyPlayerList: " + LobbyPlayerList._instance.ToString () 
+					+ " out of " + FindObjectOfType<LobbyManager> ().maxPlayers);*/
+				if (MainConditionForGhost) { 
+					//Debug.Log (playerName + " will be Ghost");
+					SetTeam (myLobbyPlayer.Teams.Ghost);
+				} else {
+					//Debug.Log (playerName + " will be Survivor");
+					SetTeam (myLobbyPlayer.Teams.Survivor);
+				}
+			} catch {
+				//Debug.Log ("Failed to get my Lobby Player");
+			}
+		} else {
+			//Debug.Log (playerName + " is connected, but I am not a server.");
+		}
+	}
+
+	bool CheckForGhost () {
+		foreach (var item in LobbyPlayerList._instance.GetListOfLobbyPlayers()) {
+			try {
+				if (item.GetComponent <myLobbyPlayer> ().GetTeam () == myLobbyPlayer.Teams.Ghost) {
+					//Debug.Log ("Ghost is found. It is " + item.playerName);
+					return true;
+				}
+			} catch {
+				//Debug.Log ("Failed to get my Lobby Player from item in _players");
+				return false;
+			}
+		}
+		//Debug.Log ("Ghost is not found");
+		return false;
+	}
+
+	//[ClientRpc]
+	public void SetTeam (Teams Team) {
+		this.team = Team;
+		//Debug.Log (playerName + " now is " + team.ToString ());
+		LobbyManager.s_Singleton.SetPlayerTypeLobby (GetComponent <NetworkIdentity> ().connectionToClient, this.team);
+	}
+
+	public Teams GetTeam () {
+		return team;
+	}
+
+	[ClientRpc]
+	public void RpcUpdateGameOverCountdown (int countdown) {
+		LobbyManager.s_Singleton.topPanel.ToggleVisibility (true);
+		LobbyManager.s_Singleton.CountdownField.text = countdown != 0 ? "Disconnect in " + countdown : "Match is in the progress.";
+	}
+}
